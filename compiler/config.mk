0b4f2dd3b2a5928ecbdfe415ab72cf7c86aed556
keyeegit <153518482+keyeegit@users.noreply.github.com>
2026-06-05 17:01:38 +0200

    Add Axel's ANTLR configuration as comments
    Added commented-out paths for Axel's ANTLR configuration.

--- compiler/config.mk
+++ compiler/config.mk
@@ -7,3 +7,9 @@
 ANTLRJAR=/root/antlr4-install/antlr-4.13.2-complete.jar
 ANTLRINC=/root/antlr4-install/include/antlr4-runtime/
 ANTLRLIB=/root/antlr4-install/lib/libantlr4-runtime.a
+
+# Axel
+# ANTLRJAR=/home/root/antlr4-install/antlr-4.13.2-complete.jar
+# ANTLRINC=/usr/local/include/antlr4-runtime
+# ANTLRLIB=/usr/local/lib/libantlr4-runtime.a
+# ANTLRLIB=/root/antlr4-install/lib/libantlr4-runtime.a
