f28d9db7feddb6dabb31d2dc0f0fefab2c612873
GuillaumeKusiak <guillaume.kusiak@gmail.com>
2026-06-05 15:02:49 +0200

    docs: update readme

--- compiler/config.mk
+++ compiler/config.mk
@@ -1,9 +1,9 @@
 # Guillaume
-# ANTLRJAR=/opt/homebrew/Cellar/antlr/4.13.2/antlr-4.13.2-complete.jar
-# ANTLRINC=/opt/homebrew/opt/antlr4-cpp-runtime/include/antlr4-runtime
-# ANTLRLIB=/opt/homebrew/opt/antlr4-cpp-runtime/lib/libantlr4-runtime.a
+ANTLRJAR=/opt/homebrew/Cellar/antlr/4.13.2/antlr-4.13.2-complete.jar
+ANTLRINC=/opt/homebrew/opt/antlr4-cpp-runtime/include/antlr4-runtime
+ANTLRLIB=/opt/homebrew/opt/antlr4-cpp-runtime/lib/libantlr4-runtime.a
 
 # Baptiste
-ANTLRJAR=/root/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/root/antlr4-install/include/antlr4-runtime/
-ANTLRLIB=/root/antlr4-install/lib/libantlr4-runtime.a
+# ANTLRJAR=/root/antlr4-install/antlr-4.13.2-complete.jar
+# ANTLRINC=/root/antlr4-install/include/antlr4-runtime/
+# ANTLRLIB=/root/antlr4-install/lib/libantlr4-runtime.a
